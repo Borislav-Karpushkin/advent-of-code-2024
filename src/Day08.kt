9c77d528b83509067de07c355f5bcde3a0ce4d39
Borislav Karpushkin <borislav.karpushkin@dif.tech>
2024-12-09 02:50:37 +0300

    Day 8 Tried Kotlin notebooks as well!

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,13 +1,56 @@
-import java.util.LinkedList
-
 fun main() {
 
+    fun parseAntennasCoordinates(input: List<String>): Map<Char, List<Pair<Int, Int>>> {
+        return input.flatMapIndexed { y, string ->
+            string.mapIndexed { x, ch -> ch to (x to y) }
+                .filter { it.first != '.' }
+        }.groupBy(keySelector = { pair -> pair.first }, valueTransform = { pair -> pair.second })
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        val yMax = input.size - 1
+        val xMax = input.first().length - 1
+        val antinodes = mutableSetOf<Pair<Int, Int>>()
+        parseAntennasCoordinates(input).forEach { charToCoordinates ->
+            val coordinates = charToCoordinates.value
+            for (i in 0..<coordinates.size - 1) {
+                for (j in i + 1..<coordinates.size) {
+                    val a = coordinates[i]
+                    val b = coordinates[j]
+                    val diff = (b.first - a.first) to (b.second - a.second)
+                    antinodes.add((b.first + diff.first) to (b.second + diff.second))
+                    antinodes.add((a.first - diff.first) to (a.second - diff.second))
+                }
+            }
+        }
+        return antinodes.filter { (x, y) -> x in 0..xMax && y in 0..yMax }.size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val yMax = input.size - 1
+        val xMax = input.first().length - 1
+        val antinodes = mutableSetOf<Pair<Int, Int>>()
+        val antennasCoordinates = parseAntennasCoordinates(input)
+        antennasCoordinates.forEach { charToCoordinates ->
+            val coordinates = charToCoordinates.value
+            for (i in 0..<coordinates.size - 1) {
+                for (j in i + 1..<coordinates.size) {
+                    var a = coordinates[i]
+                    var b = coordinates[j]
+                    val diff = (b.first - a.first) to (b.second - a.second)
+                    while (b.first + diff.first in 0..xMax && b.second + diff.second in 0..yMax) {
+                        b = (b.first + diff.first) to (b.second + diff.second)
+                        antinodes.add(b)
+                    }
+
+                    while (a.first - diff.first in 0..xMax && a.second - diff.second in 0..yMax) {
+                        a = (a.first - diff.first) to (a.second - diff.second)
+                        antinodes.add(a)
+                    }
+                }
+            }
+        }
+        return (antinodes + antennasCoordinates.flatMap { it.value }).distinct().size
     }
 
     val input = readInput("Day08")
