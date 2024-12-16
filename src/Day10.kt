c1d71fd81ee8468c1d8448f052500c4f2aca3829
Borislav Karpushkin <borislav.karpushkin@dif.tech>
2024-12-16 15:12:41 +0300

    Day 10 not hard

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,16 +1,50 @@
-import java.util.LinkedList
-
 fun main() {
 
+    val destinations = listOf(1 to 0, -1 to 0, 0 to 1, 0 to -1)
+
+    fun findTrailHeads(heights: List<List<Int>>, x: Int, y: Int, height: Int): List<Pair<Int, Int>> {
+        if (y !in 0 ..< heights.size) return listOf()
+        if (x !in 0 ..< heights[y].size) return listOf()
+        if (heights[y][x] != height) return listOf()
+        if (height == 9) {
+            return listOf(x to y)
+        }
+
+        return destinations.map { (xDiff, yDiff) ->
+            findTrailHeads(heights, x + xDiff, y + yDiff, height + 1,)
+        }.reduce { acc, next -> acc + next }
+    }
+
+
     fun part1(input: List<String>): Int {
-        return 0
+        val heights = input.map { string -> string.map { ch -> ch.digitToInt() } }
+
+        return heights.mapIndexed { y, ints ->
+            ints.mapIndexed { x, n ->
+                if (n == 0) {
+                    findTrailHeads(heights, x, y, 0).distinct().size
+                } else {
+                    0
+                }
+            }.sum()
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val heights = input.map { string -> string.map { ch -> ch.digitToInt() } }
+
+        return heights.mapIndexed { y, ints ->
+            ints.mapIndexed { x, n ->
+                if (n == 0) {
+                    findTrailHeads(heights, x, y, 0).size
+                } else {
+                    0
+                }
+            }.sum()
+        }.sum()
     }
 
-    val input = readInput("Day08")
+    val input = readInput("Day10")
     part1(input).println()
     part2(input).println()
 }
---
+++ src/Day11.kt
@@ -0,0 +1,16 @@
+import java.util.LinkedList
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val input = readInput("Day11")
+    part1(input).println()
+    part2(input).println()
+}
